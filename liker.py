61192e5457ff78e489d734d81c2d48dce53eec1d
Kevin Kao <Kevindkao@gmail.com>
2025-12-18 20:55:19 -0600

    Adding base version

--- liker.py
+++ liker.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
 
-counter = 0
+counter = 1
 
 print("Press Ctrl+C to stop.")
 try:
@@ -10,21 +10,23 @@ try:
         start_time = time.time()
         while time.time() - start_time < 2:
             pyautogui.scroll(-100)
-            
-        time.sleep(0.5)
+
+        time.sleep(1.5)
 
         # Left click at 313, 643
-        pyautogui.click(313, 643)
+        pyautogui.click(300, 650)
+        pyautogui.click(300, 650)
         
         # Wait 1 seconds
-        time.sleep(1)
+        time.sleep(0.5)
         
         # Left click again same coordinates
-        pyautogui.click(313, 643)
-        
+        pyautogui.click(300, 630)
+        pyautogui.click(300, 630)
+
         # Wait 2.5 seconds
         print("Like " + str(counter))
         counter += 1
-        time.sleep(2.5)
+        time.sleep(1.5)
 except KeyboardInterrupt:
     print("\nStopped.")
